39eb286246c18b4700eed48bf7748b512d3dbb72
tomi <tomisin.o@softcom.ng>
2020-04-13 00:10:19 +0100

    Initial Commit

---
+++ NetworkTemplate/NetworkTemplate.h
@@ -0,0 +1,19 @@
+//
+//  NetworkTemplate.h
+//  NetworkTemplate
+//
+//  Created by OYEGOKE TOMISIN on 13/04/2020.
+//  Copyright © 2020 OYEGOKE TOMISIN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkTemplate.
+FOUNDATION_EXPORT double NetworkTemplateVersionNumber;
+
+//! Project version string for NetworkTemplate.
+FOUNDATION_EXPORT const unsigned char NetworkTemplateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkTemplate/PublicHeader.h>
+
+
